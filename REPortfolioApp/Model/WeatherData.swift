30b9a88677ef72290ff95e87b37469209ea7b7a4
joanna-2023 <140013312+joanna-2023@users.noreply.github.com>
2023-07-26 10:10:46 -0600

    Weather icons.

---
+++ REPortfolioApp/Model/WeatherData.swift
@@ -0,0 +1,10 @@
+//
+//  WeatherData.swift
+//  REPortfolioApp
+//
+//  Created by Desi Nikolova on 2023-07-26.
+//
+
+import Foundation
+
+//https://weatherstack.com/?utm_source=google&utm_medium=cpc&utm_campaign=weatherstack_search_us_ca&gclid=EAIaIQobChMI5Jiaot2sgAMVZM3CBB3vjgQuEAAYAiAAEgIiHfD_BwE
