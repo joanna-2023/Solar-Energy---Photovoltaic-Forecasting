03ff0a005f6998b20a82198601353841ad7e6edc
joanna-2023 <140013312+joanna-2023@users.noreply.github.com>
2023-08-02 17:08:27 -0600

    Main chart - improvements.

--- REPortfolioApp/Controllers/ForecastAndAnalysisViewController.swift
+++ REPortfolioApp/Controllers/ForecastAndAnalysisViewController.swift
@@ -11,8 +11,7 @@ import SwiftUI
 
 class ForecastAndAnalysisViewController:UIViewController{
     var chartView:UIView?
-    @IBOutlet var chartTitle:UILabel?
-    
+
     override func viewDidLoad() {
         UIFactory.styleMainSection(section: self.view)
         let barChart = SingleBar(isOverview: false)
@@ -24,11 +23,12 @@ class ForecastAndAnalysisViewController:UIViewController{
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         var f = self.view.frame
-        f.origin = CGPoint(x: 20,y: 20)
+        f.origin = CGPoint(x: 20, y: 0)
         f.size.width = f.size.width - 40
         f.size.height = f.size.height - 40
         chartView?.frame = self.view.frame
+        
         self.view.bringSubviewToFront(chartView!)
-        self.view.bringSubviewToFront(chartTitle!)
+        //self.view.bringSubviewToFront(chartTitle!)
     }
 }
